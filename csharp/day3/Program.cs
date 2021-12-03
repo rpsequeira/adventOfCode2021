fe76093faada0b644ac15ed1f542e638e18b1103
Ricardo Sequeira <ricardoseq@gmail.com>
2021-12-03 06:26:49 +0000

    Day 3 part 1

--- csharp/day3/Program.cs
+++ csharp/day3/Program.cs
@@ -10,7 +10,7 @@ namespace adventofcode2020
         static void Main(string[] args)
         {
             Console.WriteLine("Day 3 of AoC");
-            IEnumerable<Tuple<string, int>> data = null;
+            IEnumerable<string> data = null;
             Utils.Utils.MeasureActionTime("Load", () =>
             {
                 string input = args[0];
@@ -31,40 +31,49 @@ namespace adventofcode2020
             Console.WriteLine("END");
         }
 
-        static IEnumerable<Tuple<string, int>> LoadData(string filename)
+        static IEnumerable<string> LoadData(string filename)
         {                
             var raw = Utils.Utils.ReadAllLines(filename);
-            foreach (var item in raw)
-            {
-                var l = item.Split(' ');
-                yield return new Tuple<string, int>(l[0], Int32.Parse(l[1]));
-            }
+            return raw;
         }
 
-        static int DoMagic(IEnumerable<Tuple<string, int>> input)
+        static int DoMagic(IEnumerable<string> input)
         {        
-            var x = 0;
+            var counter = new List<int>();
             var y = 0;
             foreach (var item in input)
             {
-                switch (item.Item1)
+                for (int i = 0; i < item.Length;i++)                        
                 {
-                    case "forward":
-                        x += item.Item2;
-                        break;
-                    case "down":
-                        y += item.Item2;
-                        break;
-                    case "up":
-                        y -=item.Item2;
-                        break;
+                    if(counter.Count() <= i){
+                        counter.Add(0);
+                    }
+                    if(item[i] == '0'){
+                        counter[i]++;
+                    }
                 }
             }
 
-            return x * y;
+            var gamma = "";            
+            foreach (var item in counter)
+            {
+                if(item > (input.Count()/2)){
+                    gamma += "0";
+                }
+                else
+                {
+                    gamma += "1";
+                }
+            }
+
+            var gammaInt = Convert.ToInt32(gamma, 2);
+            var epsilon = Convert.ToString( ~gammaInt, 2)[^gamma.Length..];
+            var epsilonInt = Convert.ToInt32(epsilon, 2);
+            
+            return gammaInt * epsilonInt;
         }
 
-        static int DoMagic2(IEnumerable<Tuple<string, int>> input)
+        static int DoMagic2(IEnumerable<string> input)
         {
             var x = 0;
             var y = 0;
