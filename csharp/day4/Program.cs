ea4a695d36a8992a52396f7f2613580439e0daaf
Ricardo Sequeira <ricardo.sequeira@outsystems.com>
2021-12-03 11:55:18 +0000

    setup day 4

---
+++ csharp/day4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 4 of AoC");
+            IEnumerable<string> data = null;
+            Utils.Utils.MeasureActionTime("Load", () =>
+            {
+                string input = args[0];
+                Console.WriteLine($"Loading data...");
+                data = LoadData(input);
+                Console.WriteLine($"Length -> {data.Count()}");
+            });
+            Utils.Utils.MeasureActionTime("Part 1", () =>
+            {
+                var res = DoMagic(data);
+                Console.WriteLine($"Result -> {res}");
+            });
+            // Utils.Utils.MeasureActionTime("Part 2", () =>
+            // {
+            //     var res = DoMagic2(data);
+            //     Console.WriteLine($"Result -> {res}");
+            // });
+            Console.WriteLine("END");
+        }
+
+        static IEnumerable<string> LoadData(string filename)
+        {                
+
+        }
+
+        static int DoMagic(IEnumerable<string> input)
+        {        
+
+        }
+
+        static int DoMagic2(IEnumerable<string> input)
+        {
+        
+        }
+
+    }
+}
